9755b9ab9156c8319ae16fc108aeb381043c1fba
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2020-02-25 20:49:13 -0800

    03 - Create Atomic and Branch

--- PluginDevelopment/Plugins/StateMachine/Source/StateMachine/Public/SM_State.h
+++ PluginDevelopment/Plugins/StateMachine/Source/StateMachine/Public/SM_State.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// K-B State Machine 2020
 
 #pragma once
 
@@ -24,7 +24,7 @@ enum class EStateMachineCompletionType : uint8
 };
 
 USTRUCT()
-struct FStateMachineResult
+struct STATEMACHINE_API FStateMachineResult
 {
 	GENERATED_BODY()
 
@@ -37,6 +37,44 @@ struct FStateMachineResult
 	UPROPERTY()
 	int32 DataIndex;
 };
+
+UCLASS()
+class STATEMACHINE_API USM_InputAtom : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	// Display value for this input atom, mainly for debugging purposes
+	UPROPERTY(EditAnywhere)
+	FName Description;
+
+};
+
+UClASS(EditInNewLine)
+class STATEMACHINE_API USM_Branch : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	/* Returns DestinationState on success, NULL on failure.
+	   For subclass, OutDataIndex might be something other than 1,
+	   if branch is made to consume multiple inputs */
+	virtual USM_State* TryBranch(const UObject* RefObject, const TArray<USM_InputAtom*>& DataSource,
+		int32 DataIndex, int32 &OutDataIndex)
+
+protected:
+	// State where we will go next if this branch is taken, If null, this branch will be ignored.
+	UPROPERTY(EditAnywhere)
+	USM_State DestinationState;
+
+	// If true, the meaning of AcceptableInputs is reversed.
+	UPROPERTY(EditAnywhere)
+		uint32 bReverseInputTest : 1;
+
+	// Acceptable Inputs. The current input atom must be on this list.
+	UPROPERTY(EditAnywhere)
+		TArray<USM_InputAtom*> AcceptableInputs;
+}
 
 /**
  * 
